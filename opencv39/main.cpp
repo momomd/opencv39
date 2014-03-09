5e062852d09d9827f0010e8d32d23bf13c39d67d
Uran Oh <uranoh@Urans-MacBook-Air.local>
2014-03-09 15:27:12 -0400

    Initial Commit

---
+++ opencv39/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  opencv39
+//
+//  Created by Uran Oh on 3/9/14.
+//  Copyright (c) 2014 Uran Oh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
